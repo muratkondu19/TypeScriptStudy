607ef909f945912052bf6a12e9f4011d92ff518c
Murat Kondu <murat.kondu.mk@hotmail.com>
2022-11-27 00:00:06 +0300

    nodemon hakkında

--- intro.ts
+++ intro.ts
@@ -15,3 +15,9 @@ function selamVerTS(isim:string){
 }
 let mesajTS = selamVer("Murat")//Farklı bir türden değer verilemiyor, tanımlanan türe göre verilebiliyor
 //TSC: typescirt compiler ile compile ederek js çıktısını dosya olarak görebiliriz.
+
+//Nodemon
+// paketler kayıt olduğu zaman izler, js dosyasında değişiklik olunca paketi izleyip onu tekrardan çalıştırması gerekiyorsa çalıştırır
+//kaydet tuşuna basınca paket çalışır.
+// tsc intro.ts ile js dosyası oluşturulur
+//nodemon intro.js ile ilgili dosya çalıştırılır ve değşiklik olduğunda sonucu gözlemlenir.
